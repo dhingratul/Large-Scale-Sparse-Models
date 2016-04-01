b3767deb103af6cad44b89859546a44986effa2b
dhingratul <atul.dhingra@rutgers.edu>
2016-04-01 09:25:58 -0400

    sub-gradient method

---
+++ subgrad.m
@@ -0,0 +1,24 @@
+function [ w ] = subgrad( X, Y, lambda )
+% min_w (1/2n) || Y - Xw ||^2 + lambda || w ||_1
+
+max_iter = 1e5;
+eta = 2;
+
+[n, p] = size(X);
+
+%idx = randi(n, max_iter);
+
+w = zeros(p, 1);
+
+for iter=1:max_iter
+%     rand_idx = idx(iter);
+%     x = X(rand_idx, :);
+%     y = Y(rand_idx);
+    
+    grad = X' * (X * w - Y) / n + lambda * sign(w);
+    
+    w = w - eta / iter * grad;
+end
+
+end
+
