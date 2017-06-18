992f9b06334dc727c73b0531b36dcc3428431a95
dhingratul <atul.dhingra@rutgers.edu>
2017-06-18 18:49:33 -0400

    Documentation

--- run_Subgrad.m
+++ run_Subgrad.m
@@ -1,3 +1,4 @@
+%% Run Sub-gradient Descent
 clear;
 
 load('gisette.mat', 'X_train', 'Y_train', 'X_test', 'Y_test');
--- subgrad.m
+++ subgrad.m
@@ -1,3 +1,7 @@
+%% Sub-gradient Descent
+% Input: X, Y, Lambda
+% Output: W
+% Note: Run from run_Subgrad.m
 function [ w ] = subgrad( X, Y, lambda )
 % min_w (1/2n) || Y - Xw ||^2 + lambda || w ||_1
 
@@ -11,9 +15,9 @@ eta = 2;
 w = zeros(p, 1);
 
 for iter=1:max_iter
-%     rand_idx = idx(iter);
-%     x = X(rand_idx, :);
-%     y = Y(rand_idx);
+    %     rand_idx = idx(iter);
+    %     x = X(rand_idx, :);
+    %     y = Y(rand_idx);
     
     grad = X' * (X * w - Y) / n + lambda * sign(w);
     
