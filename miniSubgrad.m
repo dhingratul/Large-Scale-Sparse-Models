f779064833472d8594b7f10583bdf0ce26fe5a98
dhingratul <atul.dhingra@rutgers.edu>
2017-06-18 18:55:57 -0400

    Documentation

--- miniSubgrad.m
+++ miniSubgrad.m
@@ -1,3 +1,8 @@
+%% Mini-batch Sub-gradient Descent
+% Input: X, Y, Lambda, batch-Size
+% Output: W
+% Note: Run from run_Subgrad_minibatch.m
+
 function [ w ] = miniSubgrad( X, Y, lambda, batchSize)
 % min_w (1/2n) || Y - Xw ||^2 + lambda || w ||_1
 
@@ -12,9 +17,9 @@ w = zeros(p, 1);
 
 for iter=1:max_iter
     rand_idx=randi(n,[batchSize,1]);
-%     rand_idx = idx(iter);
-%     x = X(rand_idx, :);
-%     y = Y(rand_idx);
+    %     rand_idx = idx(iter);
+    %     x = X(rand_idx, :);
+    %     y = Y(rand_idx);
     
     grad = X(rand_idx,:)' * (X(rand_idx,:) * w - Y(rand_idx)) / batchSize + lambda * sign(w);
     
