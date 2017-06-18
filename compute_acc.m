af7846f6c1e0295c0ace8cfdc34d6fb953d642a7
dhingratul <atul.dhingra@rutgers.edu>
2017-06-18 18:39:48 -0400

    Documentation

--- compute_acc.m
+++ compute_acc.m
@@ -1,3 +1,7 @@
+%% Compute the accuracy of the algorithm
+% Input: X, Y, w
+% Output: accuracy
+% Note: Run from Run_ files
 function [ acc ] = compute_acc( X, Y, w )
 %check the sign of X*w and Y
 
